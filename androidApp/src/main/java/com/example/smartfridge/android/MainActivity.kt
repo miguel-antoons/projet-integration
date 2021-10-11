78b08456526d9e31adaa733a9046b183c4b59c93
Eliott Lepage <HE201841@students.ephec.be>
2021-10-11 13:31:57 +0200

    Création du fragment home, ajout du button dans ce fragment

--- androidApp/src/main/java/com/example/smartfridge/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/smartfridge/android/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.smartfridge.Greeting
 import android.widget.TextView
+import com.example.smartfridge.android.fragments.Home_Fragment
 
 fun greet(): String {
     return Greeting().greeting()
@@ -14,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inject the fragment into our box (fragment_button)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_button, Home_Fragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
---
+++ androidApp/src/main/java/com/example/smartfridge/android/fragments/Home_Fragment.kt
@@ -0,0 +1,17 @@
+package com.example.smartfridge.android.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.smartfridge.android.R
+
+class Home_Fragment : Fragment() {
+
+    // When Home_Fragment is created, it will call onCreateView -> inject fragment_home on the page
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater?.inflate(R.layout.fragment_home, container, false)
+    }
+}
