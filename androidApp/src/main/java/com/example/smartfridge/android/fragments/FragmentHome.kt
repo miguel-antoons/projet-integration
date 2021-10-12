5487d5b09c11fb0e817380a7b2033ca616d17c18
Mathis-Dory <m.dory@students.ephec.be>
2021-10-12 18:39:36 +0200

    ADD: "Connect SmartFridge" button on Homme screen
    US: https://trello.com/c/Y1xCh1Pr/29-us-page-connecter-un-smartfridge

--- androidApp/src/main/java/com/example/smartfridge/android/fragments/FragmentHome.kt
+++ androidApp/src/main/java/com/example/smartfridge/android/fragments/FragmentHome.kt
@@ -1,17 +1,27 @@
 package com.example.smartfridge.android.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.smartfridge.android.HowToConnect
 import com.example.smartfridge.android.R
 
 class FragmentHome : Fragment() {
-
     // When Home_Fragment is created, it will call onCreateView -> inject fragment_home on the page
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater?.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonHowTo: Button = view.findViewById(R.id.fragment_button)
+        buttonHowTo.setOnClickListener{
+            val intent = Intent(context, HowToConnect::class.java);
+            startActivity(intent);
+        }
     }
 }
