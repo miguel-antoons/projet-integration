385db43540fcd4f14a3e8697f772e1864a514032
Eliott Lepage <HE201841@students.ephec.be>
2021-10-13 14:21:47 +0200

    Ajout de la section en dessous du bouton 'connecter un SmartFridge'

--- androidApp/src/main/java/com/example/smartfridge/android/fragments/FragmentHome.kt
+++ androidApp/src/main/java/com/example/smartfridge/android/fragments/FragmentHome.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.smartfridge.android.R
+import com.example.smartfridge.android.adapter.FridgesAdapter
 
 class FragmentHome : Fragment() {
 
     // When Home_Fragment is created, it will call onCreateView -> inject fragment_home on the page
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater?.inflate(R.layout.fragment_home, container, false)
+        val view = inflater?.inflate(R.layout.fragment_home, container, false)
+
+        // récupérer le recyclerview
+        val verticalRecyclerView = view.findViewById<RecyclerView>(R.id.vertical_recycler_view)
+        verticalRecyclerView.adapter = FridgesAdapter()
+
+        return view
     }
 }
