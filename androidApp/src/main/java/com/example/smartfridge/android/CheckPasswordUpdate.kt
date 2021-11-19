d96d8d622bb5053157a0dbaadacee5dd988e3576
BentahriMerwane <64090979+BentahriMerwane@users.noreply.github.com>
2021-11-19 16:08:25 +0100

    EDIT : Update password
    Check if password strenght is OK

---
+++ androidApp/src/main/java/com/example/smartfridge/android/CheckPasswordUpdate.kt
@@ -0,0 +1,11 @@
+package com.example.smartfridge.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckPasswordUpdate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_password_update)
+    }
+}
