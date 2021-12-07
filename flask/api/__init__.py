e74b0a1be11abf4bba2fa798fe925de696444dc8
Maxime <max@interplanet.be>
2021-12-07 19:21:43 +0100

    DELETE : delete client and signIn file
    delete client and signIn file because unnecessary

--- flask/api/__init__.py
+++ flask/api/__init__.py
@@ -1,7 +1,5 @@
-from .client import app_client
 from .food import app_food
 from .hello_world import app_hw
-from .signIn import signIn
 from .users import getUsers
 from .login import getUser
 from .signUp import addUser
@@ -11,10 +9,8 @@ from .signUp import addUser
 
 # each time a new blue print is registerd, it MUST be added too this file the same way the others are !!!
 blueprints = [
-    app_client,
     app_food,
     app_hw,
-    signIn,
     getUsers,
     getUser,
     addUser,
