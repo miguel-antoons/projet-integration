ed927fb4f960aa5b18e5110a3ad8a84b5f3eed41
BentahriMerwane <64090979+BentahriMerwane@users.noreply.github.com>
2021-12-09 21:31:21 +0100

    ADD : Flask dockerfile and Button to reset password pages I add my dockerfile Flask + app.ini(wsgi gunicorn) and Back button in the password change process pages

--- flask/api/database.py
+++ flask/api/database.py
@@ -7,9 +7,6 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client['SmartFridge']
 users = db["Users"]
 food = db["Food"]
-new_user = db["NewUser"]
 raspberry = db["raspberry"]
 
 
-flask_jwt_auth = client['flask_jwt_auth']
-flask_jwt_auth_coll = flask_jwt_auth["flask_jwt_auth"]
